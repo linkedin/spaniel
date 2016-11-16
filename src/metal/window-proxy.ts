46510cd6ccba2490947bb0f0d26474471e8e8faf
Brandyn Bennett <brbennett@linkedin.com>
2016-11-16 09:41:19 -0800

    Fix Window Height/Width Availability Race Condition
    The 'DOMContentLoaded' event was being fired late in bigpipe mode.
    This resulted in the height/width being set to 0.  Moved setting
    the window height/width before waiting for 'DOMContentLoaded'

--- src/metal/window-proxy.ts
+++ src/metal/window-proxy.ts
@@ -1,6 +1,6 @@
 /*
 Copyright 2016 LinkedIn Corp. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- 
+
 Unless required by applicable law or agreed to in writing, software  distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 */
 
@@ -31,11 +31,13 @@ function hasDomSetup() {
   let se = typeof (<any>document).scrollingElement !== 'undefined';
   W.getScrollTop = se ? () => (<any>document).scrollingElement.scrollTop : () => (<any>window).scrollY;
   W.getScrollLeft = se ? () => (<any>document).scrollingElement.scrollLeft : () => (<any>window).scrollX;
-  W.getHeight = () => (<any>window).innerHeight;
-  W.getWidth = () => (<any>window).innerWidth;
 }
 
 if (hasDOM) {
+  // Set the height and width immediately because they will be available at this point
+  W.getHeight = () => (<any>window).innerHeight;
+  W.getWidth = () => (<any>window).innerWidth;
+
   if ((<any>document).readyState !== 'loading') {
     hasDomSetup();
   } else {
