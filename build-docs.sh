f614a7571df9cccaf1ccb9c69dabfbff83c95ad9
asakusuma <asa.kusuma@gmail.com>
2016-12-16 15:47:51 -0800

    Scripts housekeeping

--- build-docs.sh
+++ build-docs.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Copyright 2016 LinkedIn Corp. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
 typedoc --out docs src/index.ts --module es2015 --mode file --theme minimal --readme USAGE.md
 npm run stats
 mv size.txt docs/
