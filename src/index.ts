5f0a9fbcaa5eda13af9c9f3aa472a2b1ee883940
asakusuma <asa.kusuma@gmail.com>
2017-05-10 09:13:58 -0700

    Create independent watcher and spaniel observer versions
    Create “native” versions of watcher and spaniel observer that don’t
    depend directly on the intersection observer polypill. Changes the API
    and requires passing in an intersection observer class, i.e. dependency
    injection. This doesn’t affect the public API, just adding for
    experimentation for now.

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
 import {
   SpanielIntersectionObserver,
-  entrySatisfiesRatio,
   generateEntry
 } from './intersection-observer';
+
+import {
+  entrySatisfiesRatio
+} from './utils';
 
 import {
   SpanielTrackedElement,
