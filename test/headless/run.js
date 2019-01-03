adaecd4902afa3dd9d09b26586ed9331021d935b
asakusuma <asa.kusuma@gmail.com>
2019-01-03 09:38:06 -0800

    Upgrade mocha, babel, and typescript

--- test/headless/run.js
+++ test/headless/run.js
@@ -1,6 +1,6 @@
 /*
 Copyright 2017 LinkedIn Corp. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- 
+
 Unless required by applicable law or agreed to in writing, software  distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 */
 
@@ -13,12 +13,12 @@ server.stdout.on('data', data => {
   // Wait for signal that test app has booted
   if (data.indexOf('Serving Spaniel Test App') > -1 && !test) {
     test = spawn('./node_modules/mocha/bin/mocha', [
-      '--compilers',
-      'js:babel-core/register',
+      '--require',
+      '@babel/register',
       'test/headless/specs/**/*.js',
+      '--exit',
       '--timeout',
-      '3000',
-      '--exit'
+      '5000'
     ]);
     //mocha --compilers js:babel-core/register test/headless/specs/**/*.js
 
